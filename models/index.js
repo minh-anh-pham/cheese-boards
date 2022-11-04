c3bd9a89704847f7825f7e689f7f111634fbb2a0
Minh Anh Pham <minhanhpham@fb.com>
2022-11-04 11:26:32 +0000

    add one-to-many association between user and board, many-to-many association between board and cheese

--- models/index.js
+++ models/index.js
@@ -2,6 +2,13 @@ const User = require("./user.model");
 const Cheese = require("./cheese.model");
 const Board = require("./board.model");
 
+// associations
+User.hasMany(Board);
+Board.belongsTo(User);
+
+Board.belongsToMany(Cheese, {through: "Board_Cheese"});
+Cheese.belongsToMany(Board, {through: "Board_Cheese"});
+
 module.exports = {
     User,
     Cheese,
